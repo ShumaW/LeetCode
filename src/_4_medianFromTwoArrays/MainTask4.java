ef9320a7e39edaf2ee616ed260de1f9242c6e44f
Yevgeniy_Shulimenko <evgeniy.shulimenko@gmail.com>
2023-12-27 00:19:48 +0100

    26.12.2023 refactor code Task 4

--- src/_4_medianFromTwoArrays/MainTask4.java
+++ src/_4_medianFromTwoArrays/MainTask4.java
@@ -8,7 +8,7 @@ public class MainTask4 {
      */
 
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, 5, 21,45};
+        int[] arr1 = {1, 2, 5, 21};
         int[] arr2 = {4, 8,10, 12};
 
 //        System.out.println(Arrays.toString(getMergedSortedArray(arr1, arr2)));
@@ -17,36 +17,8 @@ public class MainTask4 {
     }
 
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        double result = 0.0;
-        int length = nums1.length + nums2.length;
-        int[] mergedArray = new int[length];
-        int index1 = 0;
-        int index2 = 0;
-        for (int i = 0; i < mergedArray.length; i++) {
-            if (nums1[index1] > nums2[index2] && nums2[index2] != 0) {
-                mergedArray[i] = nums2[index2];
-                nums2[index2] = 0;
-                if (index2 < nums2.length - 1){
-                    index2++;
-                }
-            } else if (nums1[index1] < nums2[index2] && nums1[index1] != 0){
-                mergedArray[i] = nums1[index1];
-                nums1[index1] = 0;
-                if (index1 < nums1.length - 1) {
-                    index1++;
-                }
-            } else if (nums2[index2] == 0) {
-                mergedArray[i] = nums1[index1];
-                if (index1 < nums1.length - 1){
-                    index1++;
-                }
-            } else if (nums1[index1] == 0) {
-                mergedArray[i] = nums2[index2];
-                if (index2 < nums2.length - 1){
-                    index2++;
-                }
-            }
-        }
+        double result;
+        int[] mergedArray = getMergedSortedArray(nums1, nums2);
         if (mergedArray.length % 2 == 0) {
             result = (mergedArray[(mergedArray.length - 1) / 2] + mergedArray[(mergedArray.length - 1) / 2 + 1]) / 2.0;
         } else {
