9ef98ff51423038056b9c76c54fe92b64638a72c
Yevgeniy_Shulimenko <evgeniy.shulimenko@gmail.com>
2024-03-16 20:02:25 +0100

    20240216: added Task 8

--- src/_7_Reverse_Integer/Main.java
+++ src/_7_Reverse_Integer/Main.java
@@ -2,6 +2,7 @@ package _7_Reverse_Integer;
 
 public class Main {
     public static void main(String[] args) {
+
         /**
          * Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go
          * outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
